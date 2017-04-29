13c9463d3d5e0ae6f78d2d903d2da33255bc2429
DmitryZagursky <zagurski@physics.msu.ru>
2017-04-30 01:39:43 +0300

    Extended presentation

--- octave/present_latest.m
+++ octave/present_latest.m
@@ -3,13 +3,26 @@
 page_screen_output(0);
 page_output_immediately(1);%octave printing to console settings
 
-disp("Getting data");
+disp("Getting data, Dima");
 d=get_data('results/Drugie_500.txt');
-air=d(:,1:10); tim=d(:,11:20); div=d(:,21:30);
+%air=d(:,1:10); tim=d(:,11:20); div=d(:,21:30);
 disp("Counting correlations");
 cor=matr_corr(d);
-pcolor(cor);
+pcolor(cor);%Drawing and customizing plot
 grid;
-title("Correlations between the various measurements");
+title("Correlations between the various measurements, Dima");
 axis("square");
 colorbar;
+
+disp("Getting data, Nikita");
+d=get_data('results/500.txt');
+%air=d(:,1:10); tim=d(:,11:20); div=d(:,21:30);
+disp("Counting correlations");
+cor=matr_corr(d);
+figure;
+pcolor(cor);
+grid;
+title("Correlations between the various measurements, Nikita");
+axis("square");
+colorbar;
+
